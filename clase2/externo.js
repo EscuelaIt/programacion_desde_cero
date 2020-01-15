ff88c1d4b1282dfd2d8105c0cb596d9808a9ba54
Miguel Angel Alvarez <malvarez@desarrolloweb.com>
2020-01-15 20:13:46 +0100

    clase 2

---
+++ clase2/externo.js
@@ -0,0 +1,10 @@
+var numero = 5;
+alert(numero++);
+alert(numero);
+
+var multiplicando = prompt('dame un multiplicando');
+
+//numero = numero * multiplicando;
+numero *= multiplicando;
+
+alert(numero);
---
+++ clase2/hola.js
@@ -0,0 +1,2 @@
+var nombre = "miguel";
+console.log('Hola ' + nombre);
