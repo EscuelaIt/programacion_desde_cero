863ef431332dde9d44fb223f86585f528d95d5b2
Miguel Angel Alvarez <malvarez@desarrolloweb.com>
2020-01-16 19:51:58 +0100

    Ejercicios clase 2

---
+++ clase2/ejercicios/cuatro.js
@@ -0,0 +1,9 @@
+var nombre = "Miguel";
+var apellido1 = "Alvarez";
+var apellido2 = "Sánchez";
+
+// var nombreYApellidos = nombre + ' ' + apellido1 + ' ' + apellido2;
+var nombreYApellidos = `Tu nombre y apellidos son:
+${nombre} ${apellido1} ${apellido2}.`;
+console.log(nombreYApellidos);
+
