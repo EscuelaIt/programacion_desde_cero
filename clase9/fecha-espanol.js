af4f88071a1287c10f39e75ea0405a3f64ed878b
Miguel Angel Alvarez <malvarez@desarrolloweb.com>
2020-01-29 20:56:44 +0100

    Solucionar problema al setear fechas

--- clase9/fecha-espanol.js
+++ clase9/fecha-espanol.js
@@ -1,21 +1,37 @@
 class FechaEspanol {
   constructor() {
-    this.fecha = new Date(2018, 5, 6);
+    this.fecha = new Date();
   }
 
-  seteaFecha(fecha) {
+  seteaFechaIncorrecto(fecha) {
     if(fecha.anio !== undefined) {
       this.fecha.setFullYear(fecha.anio);
     }
-    console.log(fecha.mes);
-    if(fecha.mes !== undefined) {
-      this.fecha.setMonth(fecha.mes);
-    }
-    console.log('---', this.fecha.getMonth());
+    // he movido el cambio de día a esta posición para evitar lo que me pasó en la clase
+    // era lo siguiente: hoy es día 29. Al cambiar el mes primero a febrero del año 1999 (q no es bisiesto)
+    // se generaba una fecha no válida (29/02/1999) que equivale a 1 de marzo.
+    // Ahora moviendo el cambio de día antes, no me ocurre, porque primero cambio el día a 10 y luego cambio el mes y no se me va a marzo
+    // De todos modos esta manera de operar también puede dar problemas, por cambiar primero el día estando un mes que no tenga válidez
+    // Habría que cambiar todos los valores a la vez para no tener problemas de este estilo
     if(fecha.dia) {
       this.fecha.setDate(fecha.dia);
     }
+    if(fecha.mes !== undefined) {
+      this.fecha.setMonth(fecha.mes);
+    }
+  }
 
+  seteaFecha(fecha) {
+    if(fecha.anio === undefined) {
+      fecha.anio = this.fecha.getFullYear();
+    }
+    if(fecha.mes === undefined) {
+      fecha.mes = this.fecha.getMonth();
+    }
+    if(fecha.dia === undefined) {
+      fecha.dia = this.fecha.getDate();
+    }
+    this.fecha = new Date(fecha.anio, fecha.mes, fecha.dia, this.fecha.getHours(), this.fecha.getMinutes(), this.fecha.getSeconds());
   }
 
   // 29/1/2020
@@ -42,12 +58,11 @@ var fecha = new FechaEspanol();
 
 var objetoLiteralCofiguracionFecha = {
   anio: 1999,
-  dia: 10,
-  mes: 0
+  mes: 2
 }
 fecha.seteaFecha(objetoLiteralCofiguracionFecha);
 
-console.log(fecha.obtenerFechaResumida());
-console.log(fecha.obtenerFechaEscrita());
+console.log('fecha resumida: ', fecha.obtenerFechaResumida());
+console.log('fecha escrita: ', fecha.obtenerFechaEscrita());
 
 
